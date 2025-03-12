c80a995901ea68a03b429e3581131311d6061665
Roberto Costa <roberto.araujo339@gmail.com>
2025-03-12 19:47:27 -0300

    Refactor food schemas to use Pydantic Field for enhanced validation and examples

--- schemas/food.py
+++ schemas/food.py
@@ -1,17 +1,19 @@
 from datetime import datetime
-from pydantic import BaseModel
+from uuid import uuid4
+from pydantic import BaseModel, Field
 from typing import Optional
 
 
 class FoodSchema(BaseModel):
     """ Define how a food item will be returned.
     """
-    id: str
-    name: str
-    unit: str
-    calories: int
-    created_at: datetime
-    updated_at: Optional[datetime] = None
+    id: str = Field(default_factory=lambda: uuid4().hex)
+    name: str = Field(..., example="Avocado")
+    unit: str = Field(..., example="100g")
+    calories: int = Field(..., example=160)
+    created_at: datetime = Field(..., example="2025-03-12T15:06:04.573140")
+    updated_at: Optional[datetime] = Field(
+        None, example="2025-03-12T15:06:04.573140")
 
     class Config:
         from_attributes = True
@@ -20,38 +22,48 @@ class FoodSchema(BaseModel):
 class CreateFoodSchema(BaseModel):
     """ Define the schema for creating a new food item.
     """
-    name: str = "Avocado"
-    unit: str = "100g"
-    calories: int = 160
+    name: str = Field(..., example="Avocado")
+    unit: str = Field(..., example="100g")
+    calories: int = Field(..., example=160)
 
 
 class UpdateFoodSchema(BaseModel):
     """ Define the schema for updating an existing food item.
     """
-    name: Optional[str] = "Strawberry"
-    unit: Optional[str] = "50g"
-    calories: Optional[int] = 30
+    name: Optional[str] = Field(None, example="Strawberry")
+    unit: Optional[str] = Field(None, example="50g")
+    calories: Optional[int] = Field(None, example=30)
 
 
 class GetFoodSchema(BaseModel):
     """ Define the schema for getting a food item.
     """
-    id: str = "1"
+    id: str = Field(default_factory=lambda: uuid4().hex)
 
 
 class ListFoodSchema(BaseModel):
     """ Define how a list of food items will be returned.
     """
-    foods: list[FoodSchema]
-
-    class Config:
-        from_attributes = True
-        json_encoders = {
-            datetime: lambda v: v.isoformat(),
-        }
+    foods: list[FoodSchema] = Field(..., example=[
+        {
+            "id": "f0dc437c-cddc-49fb-8d52-6d15e44ba6cc",
+            "name": "Avocado",
+            "unit": "100g",
+            "calories": 160,
+            "created_at": "2025-03-12T15:06:04.573140",
+            "updated_at": "2025-03-12T15:06:04.573140"
+        },
+        {
+            "id": "f0dc437c-cddc-49fb-8d52-6d15e44ba6cc",
+            "name": "Strawberry",
+            "unit": "50g",
+            "calories": 30,
+            "created_at": "2025-03-12T15:06:04.573140",
+            "updated_at": "2025-03-12T15:06:04.573140"
+        }])
 
 
 class DeleteFoodSchema(BaseModel):
     """ Define the schema for deleting a food item.
     """
-    id: str = "1"
+    id: str = Field(default_factory=lambda: uuid4().hex)
